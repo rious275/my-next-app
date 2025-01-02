abbdeb377c478cff32ff4a69c2fe1c3aba0202ec
choiminj <mjchoi@studiomate.kr>
2025-01-02 11:20:39 +0900

    이미지

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
-
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+if (process.env.NODE_ENV === "development") {
+  setupDevPlatform()
+    .then(() => {
+      // Development platform setup complete
+    })
+    .catch(error => {
+      console.error("Failed to setup development platform:", error);
+    });
+}
 
 export default nextConfig;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,7 @@ const Test = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Link href="/">누르면 홈으로</Link>
+      <Link href={`/test/1`}>상세 페이지로</Link>
     </div>
   );
 };
